70d33b7c2961132e8c141a8c37bf50b7e64ffadb
swaroo2609 <swaroosumathi@gmail.com>
2024-06-19 19:30:48 +0530

    Add files via upload

---
+++ code1.js
@@ -0,0 +1,15 @@
+allow()
+{
+    var ps=document.getElementById("menu1");
+    ps.childNodes[1].innerHTML="no.of.orders taken is:0";
+
+}
+
+class Show
+{
+    Render()
+    {
+        
+    }
+    Render("<Show/>")
+}
